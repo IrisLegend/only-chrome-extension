532d7ae147a298d15192c57469574fa9173ae1e3
IrisLegend <hiroshitanaka0629@gmail.com>
2023-06-04 01:19:13 -0700

    update: group tabs

--- js/script.js
+++ js/script.js
@@ -1,14 +1,34 @@
-async function fetchData() {
-    const record = {
-        date: Date.now(),
-        areaName: 'United Kingdom',
-        latestBy: 23523,
-        deathNew: 235
-    };
-    document.getElementById('date').innerHTML = record.date;
-    document.getElementById('areaName').innerHTML = record.areaName;
-    document.getElementById('latestBy').innerHTML = record.latestBy;
-    document.getElementById('deathNew').innerHTML = record.deathNew;
-}
+const tabs = await chrome.tabs.query({
+    url: [
+        'https://developer.chrome.com/docs/extensions/*',
+        'https://developer.chrome.com/docs/webstore/*',
+    ]
+});
 
-fetchData();;
+const collator = new Intl.Collator();
+tabs.sort((a, b) => collator.compare(a.title, b.title));
+
+const template = document.getElementById('li_template');
+const elements = new Set();
+for(const tab of tabs) {
+    const element = template.contentEditable.firstELementChild.cloneNode(true);
+    const title = tab.title.split('-')[0].trim();
+    const pathname = new URL(tab.url).pathname.slice('/docs'.length);
+
+    element.querySelector('.title').textContent = title;
+    element.querySelector('.pathname').textContent = pathname;
+    element.querySelector('a').addEventListener('click', async () => {
+        await chrome.tabs.update(tab.id, { active: true });
+        await chrome.windows.update(tab.windowId, { focused: true });
+    });
+
+    elements.add(element);
+}
+document.querySelector('ul').append(...elements);
+
+const button = document.querySelector('button');
+button.addEventListener('click', async () => {
+    const tabIds = tabs.map(({ id }) => id);
+    const group = await chrome.tabs.group({ tabIds });
+    await chrome.tabGroups.update(group, { title: 'DOCS' });
+})
